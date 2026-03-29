db52f6d0bacf5ed81d15d27542e2fefcd4d8f559
kus0023 <utsav.singh0023@gmail.com>
2026-03-29 16:46:39 +0530

    Part 1: Player creation (https://www.youtube.com/watch?v=DHgj5jhMJKg&list=PLjcN1EyupaQm20hlUE11y9y8EY2aXLpnv&index=1&pp=iAQB)

--- src/main.py
+++ src/main.py
@@ -1,20 +1,41 @@
 import pygame
+
 pygame_variable = pygame.init()
 print("pygame initialised", pygame_variable)
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = SCREEN_WIDTH * 0.8
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Shooter Game")
 
-x=200
-y=200
-img = pygame.image.load("assets/img/player/Idle/0.png").convert()
+
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("src/assets/img/player/Idle/0.png").convert()
+        self.image = pygame.transform.scale(
+            img,
+            (int(img.get_width() * scale), int(img.get_height() * scale)),
+        )
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
+x = 200
+y = 200
+scale = 3
+soldier = Soldier(x, y, scale)
 
 run = True
 while run:
-    
+
+    soldier.draw()
+    pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
